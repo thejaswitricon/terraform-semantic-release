ad3802e3ef6b57e2c7fd8dec83e357e443fee0f6
thejaswitricon <thejaswi.cy@triconinfotech.com>
2023-07-04 12:58:24 +0530

    fix(root): initialize repo

--- semantic-release.sh
+++ semantic-release.sh
@@ -5,7 +5,8 @@ dirs=$(git diff-tree --no-commit-id --name-only -r ${{ github.sha }} | awk -F/ '
 echo "::set-output name=dirs::$dirs"
 
 # Check if there are any tags available
-if [[ -z $(git tag -l) ]]; then
+has_tags=$(git tag -l | wc -l)
+if [[ $has_tags -eq 0 ]]; then
   echo "::set-output name=has_tags::false"
 else
   echo "::set-output name=has_tags::true"
@@ -16,14 +17,13 @@ app_name=$(echo "$dirs" | awk '{print tolower($0)}')
 echo "::set-output name=tag_format::${app_name}-v\${version}"
 
 # Determine the path to the CHANGELOG.md file dynamically
-app_dirs="$dirs"
 path=""
 while IFS= read -r dir; do
   if [[ -f "${dir}/CHANGELOG.md" ]]; then
     path="${dir}/CHANGELOG.md"
     break
   fi
-done <<< "$app_dirs"
+done <<< "$dirs"
 
 if [[ -n "$path" ]]; then
   echo "::set-output name=path::$path"
