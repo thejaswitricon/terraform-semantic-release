9494e06159c15388077db8719eb7060bc27ab8db
thejaswitricon <thejaswi.cy@triconinfotech.com>
2023-07-04 12:52:02 +0530

    fix(root): initialize repo

--- semantic-release.sh
+++ semantic-release.sh
@@ -2,29 +2,29 @@
 
 # Get the name of the directory that was changed
 dirs=$(git diff-tree --no-commit-id --name-only -r ${{ github.sha }} | awk -F/ 'BEGIN{OFS=FS} NF--')
+echo "::set-output name=dirs::$dirs"
 
 # Check if there are any tags available
 if [[ -z $(git tag -l) ]]; then
-  has_tags=false
+  echo "::set-output name=has_tags::false"
 else
-  has_tags=true
+  echo "::set-output name=has_tags::true"
 fi
 
 # Update the tag format to include the name of the directory
-APP_NAME=$(echo "$dirs" | awk '{print tolower($0)}')
-tag_format="${APP_NAME}-v\${version}"
+app_name=$(echo "$dirs" | awk '{print tolower($0)}')
+echo "::set-output name=tag_format::${app_name}-v\${version}"
 
 # Determine the path to the CHANGELOG.md file dynamically
-changelog_path=""
+app_dirs="$dirs"
+path=""
 while IFS= read -r dir; do
   if [[ -f "${dir}/CHANGELOG.md" ]]; then
-    changelog_path="${dir}/CHANGELOG.md"
+    path="${dir}/CHANGELOG.md"
     break
   fi
-done <<< "$dirs"
+done <<< "$app_dirs"
 
-# Output variables
-echo "::set-output name=dirs::$dirs"
-echo "::set-output name=has_tags::$has_tags"
-echo "::set-output name=tag_format::$tag_format"
-echo "::set-output name=changelog_path::$changelog_path"
+if [[ -n "$path" ]]; then
+  echo "::set-output name=path::$path"
+fi
