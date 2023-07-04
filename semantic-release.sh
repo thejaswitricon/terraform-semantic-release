f30651c241bf6872fe8727685ca727f4364fedcc
thejaswitricon <thejaswi.cy@triconinfotech.com>
2023-07-04 14:34:41 +0530

    fix(root): initialize repo

--- semantic-release.sh
+++ semantic-release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Get the name of the directory that was changed
-dirs=$(git diff-tree --no-commit-id --name-only -r ${{ github.sha }} | awk -F/ 'BEGIN{OFS=FS} NF--' | cut -d/ -f1 | uniq)
+dirs=$(git diff-tree --no-commit-id --name-only -r $GITHUB_SHA | awk -F/ 'BEGIN{OFS=FS} NF--' | cut -d/ -f1 | uniq)
 echo "::set-output name=dirs::$dirs"
