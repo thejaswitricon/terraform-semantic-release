f36f0ac4f80597f50ab6a2d0cc2c737c95b4fade
thejaswitricon <thejaswi.cy@triconinfotech.com>
2023-07-04 14:25:25 +0530

    fix(root): initialize repo

--- semantic-release.sh
+++ semantic-release.sh
@@ -1,30 +1,5 @@
 #!/bin/bash
 
 # Get the name of the directory that was changed
-dirs=$(git diff-tree --no-commit-id --name-only -r ${{ github.sha }} | awk -F/ 'BEGIN{OFS=FS} NF--')
+dirs=$(git diff-tree --no-commit-id --name-only -r ${{ github.sha }} | awk -F/ 'BEGIN{OFS=FS} NF--' | cut -d/ -f1 | uniq)
 echo "::set-output name=dirs::$dirs"
-
-# Check if there are any tags available
-has_tags=$(git tag -l | wc -l)
-if [[ $has_tags -eq 0 ]]; then
-  echo "::set-output name=has_tags::false"
-else
-  echo "::set-output name=has_tags::true"
-fi
-
-# Update the tag format to include the name of the directory
-app_name=$(echo "$dirs" | awk '{print tolower($0)}')
-echo "::set-output name=tag_format::${app_name}-v\${version}"
-
-# Determine the path to the CHANGELOG.md file dynamically
-path=""
-while IFS= read -r dir; do
-  if [[ -f "${dir}/CHANGELOG.md" ]]; then
-    path="${dir}/CHANGELOG.md"
-    break
-  fi
-done <<< "$dirs"
-
-if [[ -n "$path" ]]; then
-  echo "::set-output name=path::$path"
-fi
