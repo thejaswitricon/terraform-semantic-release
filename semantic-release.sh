ff962ad64ab8d3f118510fd8483901816758c3f9
thejaswitricon <thejaswi.cy@triconinfotech.com>
2023-07-05 22:13:46 +0530

    feat(root): initialize a demo folder

--- semantic-release.sh
+++ semantic-release.sh
@@ -3,3 +3,4 @@
 # Get the name of the directory that was changed
 dirs=$(git diff-tree --no-commit-id --name-only -r $GITHUB_SHA | awk -F/ 'BEGIN{OFS=FS} NF--' | cut -d/ -f1 | uniq)
 echo "::set-output name=dirs::$dirs"
+
